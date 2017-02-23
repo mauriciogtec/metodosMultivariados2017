895a60f739bd07152ac7480f490569c1dd7100db
Mauricio Benjamin Garcia Tec <garciatec.mauricio@outlook.com>
2017-02-22 19:40:24 -0600

    agregar datos

---
+++ R/datasets.R
@@ -0,0 +1,6 @@
+#' @title datos de votaciones del senado
+#' @description datos de votaciones del senado
+#' @format primeras tres columnas informacion de votaciones
+#' las siguientes votaciones de cada senador
+#' 1 es que voto que si
+"senado_votaciones"
