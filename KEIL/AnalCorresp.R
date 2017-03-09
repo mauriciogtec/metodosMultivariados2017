1c552a1df8a122fe6b4b3a04b0be69b2df3c740f
stu-keil <stephanekeil@gmail.com>
2017-03-08 21:38:33 -0600

    Sesion 8

--- KEIL/AnalCorresp.R
+++ KEIL/AnalCorresp.R
@@ -29,3 +29,42 @@ par(mfrow=c(1,1))
 
 chisq.test(HairEye) 
 curve(dchisq(x,df=9),from=0,to=20)
+
+
+prop.table(HairEye)-(prop.table(margin.table(HairEye,1)) %*% t(prop.table(margin.table(HairEye,2))))
+
+
+x <- as.matrix(df.dummies[,1:4])
+y <- as.matrix(df.dummies[,5:8])
+
+### Obtener tabla de contingencia 
+P <- t(x) %*% y
+
+### Probabilidad marginal
+t(x) %*% x
+t(y) %*% y
+
+### Probabilidad condicional
+
+P1 <- P / sum(as.numeric(P))
+
+apply(P1,c(1,2),scales::percent)
+
+#### Perfiles por renglon, probabilidad de tener color de pelo si tengo algun color de ojos
+#### P(ojos|pelo)
+Dr <- t(x) %*% x /sum(as.numeric(P))
+apply(Dr,c(1,2),scales::percent)
+
+Drinv <- diag(1/diag(Dr))
+dimnames(Drinv) <- dimnames(Dr)
+Pr <- Drinv %*% P1
+apply(Pr,c(1,2),scales::percent)
+
+#Prob(pelo|ojos)
+Dc <- t(y) %*% y /sum(as.numeric(P))
+apply(Dc,c(1,2),scales::percent)
+
+Dcinv <- diag(1/diag(Dc))
+dimnames(Dcinv) <- dimnames(Dc)
+Pc <- P1 %*% Dcinv
+apply(Pc,c(1,2),scales::percent)
